b2fe1716e9813849ef6a3045a783775a8859318a
Likem <likemdzokoto@gmail.com>
2022-07-07 03:34:28 +0000

    fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,10 @@ require("@nomiclabs/hardhat-waffle");
 
 require("dotenv").config();
 
+const privatekey = process.env.PRIVATE_KEY;
+const endpoint = process.env.URL;
+
+
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
