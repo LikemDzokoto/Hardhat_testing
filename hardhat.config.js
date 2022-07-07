50511109f85df4d9e30a65875d748f13572cd0ca
Likem <likemdzokoto@gmail.com>
2022-07-07 03:39:59 +0000

    setup done

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,5 +22,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: { 
+    version: "0.8.4"
+},
+networks:{
+  rinkeby:{
+    url: endpoint,
+    accounts: [`0x${privatekey}`]
+  }
+
+}
+
 };
+
